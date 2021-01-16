374901d6034f3ea825ed1073866e7e63bcec0110
samyaa13 <74277648+samyaa13@users.noreply.github.com>
2021-01-15 18:50:03 -0800

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,19 @@
+class Ground {
+    constructor(x,y,width,height) {
+      this.image = loadImage("sprites/ground.png");
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      var pos =this.body.position;
+      imageMode(CENTER);
+      image(this.image, pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angrybirds
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
